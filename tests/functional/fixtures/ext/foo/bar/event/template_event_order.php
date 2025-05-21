36b974a7e0ae781026376b1426ebd5289b022a26
rxu <rxu@mail.ru>
2025-05-21 13:46:33 +0700

    Add template prioritizing test

---
+++ tests/functional/fixtures/ext/foo/bar/event/template_event_order.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*
+* This file is part of the phpBB Forum Software package.
+*
+* @copyright (c) phpBB Limited <https://www.phpbb.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+* For full copyright and license information, please see
+* the docs/CREDITS.txt file.
+*
+*/
+
+namespace foo\bar\event;
+
+/**
+* Event listener
+*/
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class template_event_order implements EventSubscriberInterface
+{
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'rxu.templateventsort.twig_event_tokenparser_constructor' => 'set_template_event_priority',
+		);
+	}
+
+	public function set_template_event_priority($event)
+	{
+		$event->update_subarray('template_event_priority_array', 
+			'foo_bar', [
+				'event/navbar_header_quick_links_after' => -1,
+		]);
+	}
+}
--- tests/functional/templateventsort_test.php
+++ tests/functional/templateventsort_test.php
@@ -15,9 +15,21 @@ namespace rxu\templateventsort\tests\functional;
  */
 class templateventsort_test extends \phpbb_functional_test_case
 {
+	static private $helper;
+
+	static public function setUpBeforeClass(): void
+	{
+		global $phpbb_root_path;
+
+		parent::setUpBeforeClass();
+
+		self::$helper = new \phpbb_test_case_helpers(__CLASS__);
+		self::$helper->copy_dir(__DIR__ . '/fixtures/ext/', $phpbb_root_path . 'ext/');
+	}
+
 	static protected function setup_extensions()
 	{
-		return ['rxu/templateventsort'];
+		return ['rxu/templateventsort', 'foo/bar', 'foo/foo'];
 	}
 
 	protected function setUp(): void
@@ -26,12 +38,45 @@ class templateventsort_test extends \phpbb_functional_test_case
 
 		$this->login();
 		$this->admin_login();
+
+		$this->purge_cache();
 	}
 
-	public function test_index_page()
+	/**
+	* Check a controller for extension foo/bar.
+	*/
+	public function test_template_event_order()
 	{
-		$crawler = self::request('GET', "index.php");
-		$this->assertEquals(1, $crawler->filter('div.forabg')->count());
-		$this->assertStringContainsString('Your first category', $crawler->filter('div.list-inner')->text());
+		global $phpbb_root_path;
+
+		$crawler = self::request('GET', 'index.php');
+		$quick_links_menu = $crawler->filter('ul[role="menu"]')->eq(0);
+		$quick_links_menu_nodes = $quick_links_menu->filter('li:not([class])');
+		$quick_links_menu_nodes_count = (int) $quick_links_menu_nodes->count();
+		// Ensure foo/foo template event goes before foo/bar one
+		$this->assertStringContainsString('FOO_FOO_QUICK_LINK', $quick_links_menu_nodes->eq($quick_links_menu_nodes_count - 2)->filter('span')->text());
+		$this->assertStringContainsString('FOO_BAR_QUICK_LINK', $quick_links_menu_nodes->eq($quick_links_menu_nodes_count - 1)->filter('span')->text());
+
+		// Change template events order to default, put foo/bar event before foo/foo one
+		$this->logout();
+		$this->disable_ext('foo/bar');
+		$this->disable_ext('foo/foo');
+
+		$this->assertTrue(copy(__DIR__ . '/fixtures/ext/foo/bar/event/template_event_order_higher.php', $phpbb_root_path . 'ext/foo/bar/event/template_event_order.php'));
+		$this->assertTrue(copy(__DIR__ . '/fixtures/ext/foo/foo/event/template_event_order_lower.php', $phpbb_root_path . 'ext/foo/foo/event/template_event_order.php'));
+
+		$this->install_ext('foo/bar');
+		$this->install_ext('foo/foo');
+
+		$this->login();
+		$this->admin_login();
+
+		$crawler = self::request('GET', 'index.php');
+		$quick_links_menu = $crawler->filter('ul[role="menu"]')->eq(0);
+		$quick_links_menu_nodes = $quick_links_menu->filter('li:not([class])');
+		$quick_links_menu_nodes_count = (int) $quick_links_menu_nodes->count();
+		// Ensure foo/foo template event goes before foo/bar one
+		$this->assertStringContainsString('FOO_BAR_QUICK_LINK', $quick_links_menu_nodes->eq($quick_links_menu_nodes_count - 2)->filter('span')->text());
+		$this->assertStringContainsString('FOO_FOO_QUICK_LINK', $quick_links_menu_nodes->eq($quick_links_menu_nodes_count - 1)->filter('span')->text());
 	}
 }
